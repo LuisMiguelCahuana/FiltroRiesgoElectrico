6cbeca4df2214662196b94b6a9d9dc90de0f3283
LuisMiguelCahuana <88464302+LuisMiguelCahuana@users.noreply.github.com>
2025-07-19 10:22:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import re
 
 # CONFIGURACIÓN
 login_url = "http://sigof.distriluz.com.pe/plus/usuario/login"
-FILE_ID = "1td-2WGFN0FUlas0Vx8yYUSb7EZc7MbGWjHDtJYhEY-0"
+FILE_ID = "1PCheVtYVf839zjbg5PJuyc4-_OpSvsWbjQBOi7rC258"
 headers = {
     "User-Agent": "Mozilla/5.0",
     "Referer": login_url,
